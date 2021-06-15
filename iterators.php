5fabdca3fc84ad7545d1c0ed9e5ac248c9a6bb7f
labietelabiete <victorhugogote@gmail.com>
2021-06-15 15:54:38 +0200

    print and iterators done

---
+++ iterators.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Iterators PHP</title>
+</head>
+<body>
+  <div>
+		<?php for ($i = 1; $i <= 10; $i++): ?>
+			<p><?php echo $i ?></p>
+		<?php endfor ?>
+  </div>
+  <div>
+		<?php
+			$myArray = array(1, 2, 3, 4, 5);
+			foreach($myArray as $myArrayItem) : ?>
+				<p><?php echo $myArrayItem ?></p>
+			<?php endforeach ?>
+  </div>
+  <div>
+		<?php
+			$i = 0;
+			while($i<3 ) : ?>
+				<p>
+					<?php
+						echo $i;
+						$i++;
+					?>
+				</p>
+			<?php endwhile ?>
+  </div>
+	<div>
+		<p>
+			<?php
+				$i=2;
+				do {
+					echo $i;
+					$i--;
+				} while($i > -1)
+			?>
+		</p>
+	</div>
+</body>
+</html>
---
+++ print.php
@@ -0,0 +1,45 @@
+<!-- <?php
+    $echoVariable = "Printing by echo";
+    echo $echoVariable;
+    echo '\n';
+
+    $printVariable = "Printing by print";
+    print($printVariable);
+
+    $printRVariable = "Printing by print_r";
+    print_r($printRVariable);
+?> -->
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Printing PHP</title>
+</head>
+<body>
+  <div>
+    <p>
+      <?php
+        $echoVariable = "Printing by echo";
+          echo $echoVariable;
+      ?>
+    </p>
+  </div>
+  <div>
+    <p>
+      <?php
+        $printVariable = "Printing by print";
+        print($printVariable);
+      ?>
+    </p>
+  </div>
+  <div>
+    <p>
+      <?php
+        $printRVariable = "Printing by print_r";
+        print_r($printRVariable);
+      ?>
+    </p>
+  </div>
+</body>
+</html>
