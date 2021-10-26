1c36173ce00339a7fe9420c2e7434660bac4f47d
AntonioCopete <antcopjim@gmail.com>
2021-10-26 10:23:27 +0200

    dates done

---
+++ dates.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo '<div>Today is: ' . date("d-m-Y") . '</div>';
+    echo '<div>This month is: ' . date("m") . '</div>';
+    echo '<div>It is: ' . date("s") . ' seconds</div>';
+    ?>
+</body>
+
+</html>
