54ff5956a247bdad50b5b6735de9ed6e66bfa81c
Jose1i1o <guadix88@gmail.com>
2021-10-26 20:47:23 +0200

    feat: base built

---
+++ src/iterators.php
@@ -0,0 +1,6 @@
+<?php
+    // This is using "for"
+    echo "1) This is an ", "instruction using ", "<b>echo</b> and ", "multiple variables.";
+    echo "</br>";  
+    echo "</br>";  
+    ?>
---
+++ src/print.php
@@ -0,0 +1,44 @@
+<code>
+    <div style="padding-bottom:3rem">
+        <b>1.1 Generate an instruction that uses "echo"</b><br />
+        <pre>
+<em><u>code:</u></em> echo "this is an echo";
+        </pre>
+<u>Result:</u>
+        <i>
+        <?php 
+echo 'this is an echo';
+        ?>        
+        </i>
+    </div>
+
+    <div style="padding-bottom:3rem">
+        <b>1.2 Generate an instruction that uses "print"</b><br />
+        <pre>
+<em><u>code:</u></em> print 'this is a print';
+        </pre>
+        <u>Result:</u>
+        <i>
+        <?php 
+print 'this is a print';
+        ?>        
+        </i>
+    </div>
+
+
+    <div style="padding-bottom:3rem">
+        <b>1.3 Generate an instruction that makes use of "print_r"</b><br />
+        <pre>
+<em><u>code:</u></em> $custome = array("bat", "zoombie", "monster");
+      print_r ($custome);
+        </pre>
+        <u>Result:</u>
+        <i>
+        <?php 
+            $custome = array("bat", "zoombie", "monster");
+            print_r ($custome);
+        ?>
+        </i>
+    </div>
+</code>
+
