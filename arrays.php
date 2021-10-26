ac70732a5bfa2c948ca36bc23c3be6059eb7cbb7
paulMolano <paulmolanoo@gmail.com>
2021-10-26 09:00:04 +0200

    arrays

--- arrays.php
+++ arrays.php
@@ -9,19 +9,13 @@
 </head>
 
 <body>
-
-    <div>
-        <h2>Print a text string</h2>
-        <p>
-            <?php
-            print "Printed a text string";
-            ?>
-        </p>
-    </div>
-
     <div>
         <h2>Define a simple array composed of text strings</h2>
         <p>
+            <?php
+            $cars = array("Volvo", "BMW", "Toyota");
+            echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+            ?>
 
         </p>
     </div>
@@ -29,6 +23,10 @@
     <div>
         <h2>Define a simple array consisting of whole numbers and decimal numbers</h2>
         <p>
+            <?php
+            $cars = array(1, 5.5, 6);
+            echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+            ?>
 
         </p>
     </div>
@@ -37,7 +35,13 @@
         <h2>Define a multidimensional array</h2>
         <p>
             <?php
-
+            $cars = array(
+                array("Volvo", 22, 18),
+                array("BMW", 15, 13),
+                array("Saab", 5, 2),
+                array("Land Rover", 17, 15)
+            );
+            echo $cars[0][0] . ": In stock: " . $cars[0][1] . ", sold: " . $cars[0][2] . ".<br>";
             ?>
         </p>
     </div>
@@ -46,7 +50,8 @@
         <h2>Execute the function that allows to obtain the length of an array</h2>
         <p>
             <?php
-
+            $cars = array("Volvo", "BMW", "Toyota");
+            echo count($cars);
             ?>
         </p>
     </div>
@@ -55,7 +60,10 @@
         <h2>Execute the function that allows to obtain the combination of two arrays</h2>
         <p>
             <?php
-
+            $cars = array("Volvo", "BMW", "Toyota");
+            $cars2 = array("Mercedes", "Seat", "Renault");
+            $result = array_merge($cars, $cars2);
+            echo var_dump($result);
             ?>
         </p>
     </div>
@@ -64,7 +72,8 @@
         <h2>Execute the function that once is given an array return the last element of it</h2>
         <p>
             <?php
-
+            $cars = array("Volvo", "BMW", "Toyota");
+            echo end($cars);
             ?>
         </p>
     </div>
@@ -73,7 +82,9 @@
         <h2>Execute the function that once is given an array add a new element to the array in question</h2>
         <p>
             <?php
-
+            $cars = array("Volvo", "BMW", "Toyota");
+            array_push($cars, "Mercedes");
+            echo var_dump($cars);
             ?>
         </p>
     </div>
