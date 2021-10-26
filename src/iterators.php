11565c0d94ada8f97051353f354c6edb4a4f206f
Jose1i1o <guadix88@gmail.com>
2021-10-27 00:32:45 +0200

    feat: add print, operators and iterators and styles

--- src/iterators.php
+++ src/iterators.php
@@ -1,6 +1,117 @@
-<?php
-    // This is using "for"
-    echo "1) This is an ", "instruction using ", "<b>echo</b> and ", "multiple variables.";
-    echo "</br>";  
-    echo "</br>";  
+<samp>
+<div style="padding-left: 1rem">
+    <div style="padding-bottom: 3rem">
+        <b>2.1 Generate a snippet that makes use of for</b>
+        <pre>
+<em><u>code:</u></em>
+
+<span style="color: blue;">
+for($i = 0; $i <= 5; $i++){
+echo $i;
+}
+</span>
+        </pre>
+<em><u>Result:</u></em>
+    <i>
+    <?php
+        for($i = 0; $i <= 5; $i++){
+            echo $i;
+        }
     ?>
+    </i>
+    </div>
+
+    <div style="padding-bottom: 3rem">
+        <b>2.2 Generate a snippet that makes use of foreach
+</b>
+        <pre>
+<em><u>code:</u></em> 
+
+<span style="color: blue;">$number = array(5,4,3,2,1);
+foreach($number as $i){
+$i = $i - 1;
+echo $i;
+}
+</span>
+        </pre>
+<em><u>Result:</u></em>
+    <i>
+    <?php
+        $number = array(5,4,3,2,1);
+        foreach($number as $i){
+            $i = $i - 1;
+            echo $i;
+        }
+    ?>
+    </i>
+    </div>
+
+    <div style="padding-bottom: 2rem">
+        <b>2.3 Generate a snippet that uses while</b>
+        <pre>
+<em><u>code:</u></em> 
+
+<em>option A:</em>
+<span style="color: blue;">
+    while ($i <= 5) {
+        echo $i++;
+    }
+    ?>
+</span>
+<em>option B:</em>
+<span style="color: blue;">
+$i = 0;
+while ($i <= 5):
+    echo $i;
+    $i++;
+endwhile;
+</span>
+</pre>
+<em><u>Result:</u></em>
+<i>
+<br />
+<br />
+<em>option A:</em>
+<?php
+        while ($i <= 5) {
+            echo $i++;
+        }
+        ?>
+<br />
+<em>option B:</em>
+    <?php
+        $i = 0;
+        while ($i <= 5):
+            echo $i;
+            $i++;
+        endwhile;
+    ?>
+    </i>
+    </div>
+
+    <div style="">
+        <b>2.4 Generate a snippet that uses do while</b>
+        <pre>
+<em><u>code:</u></em> 
+
+<span style="color: blue;">
+$i = 0;
+do {
+    echo $i = $i + 1;
+    $i++;
+} while ($i < 10);
+</span>
+        </pre>
+<em><u>Result:</u></em>
+    <i>
+    <?php
+$i = 0;
+do {
+    echo $i = $i + 1;
+    $i++;
+} while ($i < 10);
+    ?>
+    </i>
+    </div>
+</div>
+</code>
--- src/print.php
+++ src/print.php
@@ -1,23 +1,24 @@
-<code>
+<samp>
+<div style="padding-left: 1rem">
     <div style="padding-bottom:3rem">
         <b>1.1 Generate an instruction that uses "echo"</b><br />
         <pre>
-<em><u>code:</u></em> echo "this is an echo";
+<em><u>code:</u></em><span style="color: blue;"> echo "this is an echo";</span>
         </pre>
-<u>Result:</u>
+<em><u>Result:</u></em>
         <i>
-        <?php 
+        <?php
 echo 'this is an echo';
-        ?>        
+        ?>
         </i>
     </div>
 
     <div style="padding-bottom:3rem">
         <b>1.2 Generate an instruction that uses "print"</b><br />
         <pre>
-<em><u>code:</u></em> print 'this is a print';
+<em><u>code:</u></em><span style="color: blue;"> print 'this is a print';</span>
         </pre>
-        <u>Result:</u>
+<em><u>Result:</u></em>
         <i>
         <?php 
 print 'this is a print';
@@ -26,13 +27,13 @@ print 'this is a print';
     </div>
 
 
-    <div style="padding-bottom:3rem">
+    <div style="padding-bottom:2rem">
         <b>1.3 Generate an instruction that makes use of "print_r"</b><br />
         <pre>
-<em><u>code:</u></em> $custome = array("bat", "zoombie", "monster");
-      print_r ($custome);
+<em><u>code:</u></em><span style="color: blue;"> $custome = array("bat", "zoombie", "monster");
+      print_r ($custome);</span>
         </pre>
-        <u>Result:</u>
+<em><u>Result:</u></em>
         <i>
         <?php 
             $custome = array("bat", "zoombie", "monster");
@@ -40,5 +41,6 @@ print 'this is a print';
         ?>
         </i>
     </div>
-</code>
+</div>
+</samp>
 
