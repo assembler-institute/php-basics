fd17ca853ec998a7b471707def75dfbf384845cc
CYBER-MARCUSSEN <83762741+CYBER-MARCUSSEN@users.noreply.github.com>
2021-10-27 13:37:48 +0200

    done

---
+++ functions.php
@@ -0,0 +1,34 @@
+<!-- Create a function that given two numbers returns the sum of both -->
+</br>
+<?php  
+$x=10;  
+$y=20;  
+$z = $x + $y;  
+echo "Sum of x and y : ". $z;  
+?>
+<!-- Create a function that given two numbers returns the multiplication of both -->
+</br>
+<?php  
+$x=10;  
+$y=20;  
+$z = $x * $y;  
+echo "Sum of x and y : ". $z;  
+?>
+
+<!-- Create a function that given two numbers returns the division of both -->
+</br>
+<?php  
+$x=10;  
+$y=20;  
+$z = $x / $y;  
+echo "Sum of x and y : ". $z;  
+?>
+
+<!-- Create a function that, given two numbers and an operation (add, multiply or divide), returns the result of that operation. -->
+</br>
+<?php  
+$x=10;  
+$y=20;  
+$z = $x / $y +( $x * 2);  
+echo "Sum of x and y : ". $z;  
+?>
