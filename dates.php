4e52feeca12b688f310258b8a2aaea64f654ca02
gcachonc <g.cachon.c@gmail.com>
2021-12-14 11:10:32 +0100

    conditionals, maths, strings, types

--- dates.php
+++ dates.php
@@ -3,7 +3,7 @@
     echo $fecha -> format('Y-m-d');
     print "<br>";
     echo $fecha -> format('d')."<br>";
-    echo $fecha ->format('m')."<br>";
+    echo $fecha ->format('M')."<br>";
     echo $fecha -> format('i')."<br>";
 
 ?>
