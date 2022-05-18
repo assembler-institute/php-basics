48c117de22c0356d11ec7f4212077b2cec7064e3
Sefi Cohen <seficohen1@gmail.com>
2022-05-18 22:59:51 +0200

    :sparkles: dates in php

---
+++ dates.php
@@ -0,0 +1,25 @@
+<?php
+// dates
+
+$dateTime = new DateTime(); // getting an instance of DateTime object
+echo $dateTime->format('Y-m-d'); //formating the instance to year-month-day format
+
+echo "<br>";
+
+$currentDate = getdate();
+echo "<pre>";
+print_r($currentDate); // printing the current date as an array
+echo "</pre>";
+
+echo "<br>";
+
+$today = date('l jS \of M'); // showing the day name, the date and the month
+echo $today;
+echo "<br>";
+
+$currentMonth = date('m'); // showing month in numerical format
+echo $currentMonth;
+echo "<br>";
+
+$currentMinute = date('i'); // showing mintues with leading zeros 0-59
+echo $currentMinute;
