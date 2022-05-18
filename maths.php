171c4e6ddf861a7102fc749a482cab82252b399d
Sefi Cohen <seficohen1@gmail.com>
2022-05-18 23:06:42 +0200

    :sparkles: math methods in php
    added maths files in which several math function are being examined

---
+++ maths.php
@@ -0,0 +1,14 @@
+<?php
+
+// math functions
+
+$absoluteNumber = abs(-30); // returns the absoulte value
+
+$roundedNumber = round(23.7); // returns the rounded value of the highest next integer
+
+$maxNumber = max(22, 100, 55, 43, 99, 2); // returns the heighest number from a series of given numbers
+
+$minNumber = min(22, 100, 55, 43, 99, 2); // returns the lowest number from a series of given numbers
+
+$randNumber = rand(1, 100); // returns a random number between 1 - 100
+
