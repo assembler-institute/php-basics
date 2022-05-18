1aafc9243030a7f438c5bde9806f6ac194a1a44a
MiguelD-PC <mikdominguez@gmail.com>
2022-05-18 17:48:05 +0200

    exercises done

--- types.php
+++ types.php
@@ -1,9 +1,15 @@
 <?php
+// boolean
 $boolean = true;
+// integer
 $integer = 1;
+// float
 $float = 1.2;
+// string
 $string = "Hello world";
+// array
 $array = array(1,2,3,4);
+// object
 Class Hello{
     function helloWorld(){
         echo "Hello World";
@@ -11,4 +17,5 @@ Class Hello{
 }
 $object = new Hello;
 $object->helloWorld();
+// NULL
 $null = null;
