dc8ea5c801ec6de82a8cadf641abe0135366986d
AlejandroLHC <alejandrolhc@gmail.com>
2022-09-01 21:30:39 +0200

    add more PHP info

---
+++ conditionals.php
@@ -0,0 +1,59 @@
+<?php
+$date = new DateTime;
+if ($date->format( 'N' ) == 1) {
+    echo "We are on Monday";
+} else {
+    echo "We are not on Monday";
+}
+
+echo "<hr>";
+
+if ($date->format( 'm' ) == 10) {
+    echo "We are in October";
+} else {
+    echo "We are not in October";
+}
+
+echo "<hr>";
+
+if ($date->format( 'i' ) < 10) {
+    echo "The current minute is less than 10";
+} else if ($date->format( 'i' ) > 15) {
+    echo "The current minute is more than 15";
+} else {
+    echo "Does not meet any conditions";
+}
+
+echo "<hr>";
+
+switch ($date->format( 'N' )) {
+    case '1':
+        echo "Today is a working day";
+        break;
+
+    case '2':
+        echo "Today is a working day";
+        break;
+
+    case '3':
+        echo "Today is a working day";
+        break;
+
+    case '4':
+        echo "Today is a working day";
+        break;
+
+    case '5':
+        echo "Today is a working day";
+        break;
+
+    case '6':
+        echo "Today is not a working day";
+        break;
+
+    case '7':
+        echo "Today is not a working day";
+        break;
+}
+
+?>
---
+++ maths.php
@@ -0,0 +1,43 @@
+<?php
+$a = -3.87;
+
+$abs = abs($a);
+echo abs($abs); // 3.87
+echo "<hr>";
+
+$rou = round($a);
+echo round($rou); // -4
+echo "<hr>";
+
+function highestNum () {
+    $nums = func_get_args();
+    $highestNum = $nums[0];
+    foreach ($nums as $n) {
+        if ($n > $highestNum) {
+            $highestNum = $n;
+        }
+    }
+    return $highestNum;
+}
+$result = highestNum(2, 4, 6, 2, 7, 4, 8, 5); 
+echo $result; // <-- 8
+echo "<hr>";
+
+function lowestNum () {
+    $nums = func_get_args();
+    $lowestNum = $nums[0];
+    foreach ($nums as $n) {
+        if ($n < $lowestNum) {
+            $lowestNum = $n;
+        }
+    }
+    return $lowestNum;
+}
+$result = lowestNum(2, 4, 6, 2, 7, -4, 8, 5); 
+echo $result; // <-- -4
+echo "<hr>";
+
+$ran = rand();
+echo $ran; // <-- random integer
+
+?>
