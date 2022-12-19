b9979eb1a8b536c6da6fa350aebfd0dffb2169a0
monica-reverte <monicareverte24@gmail.com>
2022-12-19 15:44:27 +0100

    iterators

--- print.php
+++ print.php
@@ -1,8 +1,14 @@
 <?php
 
+#ECHO
+
 echo "First echo instruction<br>", "Second echo instruction<br>";
 
+#PRINT
+
 print "Print instruction <br>";
+
+#PRINT_R
 
 $letters = ['a','b','c','d','e'];
 print_r ($letters);
