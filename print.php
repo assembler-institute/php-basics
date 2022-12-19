1e9f8cf22a80e8890d15ece5decf98af3a26b898
Ruben Zafra Traver <rubenreamps@gmail.com>
2022-12-19 20:23:09 +0100

    Print File Instructions

---
+++ print.php
@@ -0,0 +1,17 @@
+<?php
+
+#Instruction with "echo" (you can add multiple lines unline print that you can only put one sentence)
+
+echo "Hello World, my name is Pepe<br>", "and I'm 28 years old.<br><br>";
+
+#Instruction with "print" 
+
+print "I like playing the guitar!<br><br>";
+
+#Instruction with print_r
+
+$array = array("Red", "Orange", "Yellow");
+
+print_r($array[1]);
+
+?>
