2868a807684c8765b864ac1a173d29f01b84bfa9
Kriiiiiiiz <kriiiiiiiz@icloud.com>
2022-12-20 11:17:02 +0100

    Added print.php

---
+++ print.php
@@ -0,0 +1,9 @@
+<?php
+echo 'Hello world! Using echo<br><br>';
+
+print 'Hello world! Using print<br><br>';
+
+$a = array ('a' => 'apple', 'b' => 'banana', 'c' => array ('x', 'y', 'z'));
+print 'Example output of print_r:<br>';
+print_r ($a);
+?>
