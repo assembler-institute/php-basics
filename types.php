0d905671715a06e134753cba3b0c1e20f5a1ead8
Miquel <abellagarciamiquel@gmail.com>
2022-12-20 09:51:22 +0100

    types file exercises done

---
+++ types.php
@@ -0,0 +1,15 @@
+<?php
+
+$isTrue = false;
+
+$number = 5;
+
+$floatNumber = 2.5;
+
+$name = 'Miquel';
+
+$fruits = ['apple', 'banana', 'orange'];
+
+$user = (object) array('name' => "Miquel", 'username' => 'mike', 'age' => 32);
+
+$nullVariable = null;
