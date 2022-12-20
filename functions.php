424b2ca777c774bc2850b09b1a0246c17460eb8f
Iuliia Shikhanova <yshihanova@gmail.com>
2022-12-20 13:31:08 +0000

    finish

--- functions.php
+++ functions.php
@@ -24,5 +24,14 @@ function all(int $a,int $b, int $c, int $d){
 }
 echo all(70, 7, 8, 6); 
 echo "<br><br>";
+
+
+$x=100;
+$y=60;
+echo "The sum of x and y is : ". ($x+$y) ."<br />";
+echo "The difference between x and y is : ". ($x-$y) ."<br />";
+echo "Multiplication of x and y : ". ($x*$y) ."<br />";
+echo "Division of x and y : ". ($x/$y) ."<br />";
+echo "Modulus of x and y : " . ($x%$y) ."<br />";
 ?>
 
--- operators.php
+++ operators.php
@@ -1,46 +1,55 @@
 <?php
 // Operators -, +, *, /, %
-function sumOfBoth(int $a, int $b){
-    return $a + $b;
-}
-echo sumOfBoth(5,7); 
-echo "<br><br>";
 
-function multOfBoth(int $a, int $b){
-    return $a * $b;
-}
-echo multOfBoth(10,7); 
+var_dump(1 + 1);
 echo "<br><br>";
-
-function divOfBoth(int $a, int $b){
-    return $a / $b;
-}
-echo divOfBoth(70,7); 
+var_dump(1 - 1);
 echo "<br><br>";
-
-function prOfBoth(int $a, int $b){
-    return $a % $b;
-}
-echo prOfBoth(70,7); 
+var_dump(1 * 2);
+echo "<br><br>";
+var_dump(10 / 5);
+echo "<br><br>";
+var_dump(10 % 1);
 echo "<br><br>";
 
 // Create a usage example for comparison operators: ==,! =, <,>, <=,> =
-$a = 4;
-$b = 3; 
-
-if($a < $b){
-    return -1;
-}elseif ($a > $b){
-    return 1;
-}elseif ($a == $b){
-    return 0;
-}elseif($a != $b){
-    return $a;
-}
+var_dump(1 == TRUE);  
+var_dump(0 != FALSE); 
+var_dump(10 > 9); 
+var_dump(7 < 8);
+echo "<br><br>";
+$hi = (object) ["hi" => "hi"]; 
+$biy = (object) ["biy" => "biy"]; 
+echo $hi <=> $biy;
+echo "<br><br>";
 
 // Create an example of use for logical operators: &&, And; ||, Or; ! (NOT); Xor
 
+$o = true && false;
+var_dump($o);
+$y = false && true;
+var_dump($y);
+$z = true && true;
+var_dump($z);
+$r = false && false;
+var_dump($r);
+$q = true || false;
+var_dump($q);
+$l = false || true;
+var_dump($l);
+$d = true || true;
+var_dump($d);
+$s = false || false;
+var_dump($s);
+echo "<br><br>";
 
+
+$var='10am';
+if($var !='12pm'){
+echo 'Get To Work';
+}else{
+echo 'Break Time!';
+}
 ?>
 
 
