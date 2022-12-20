8ee78c5f2813cb9568771e4775e70df10410fffe
Kriiiiiiiz <kriiiiiiiz@icloud.com>
2022-12-20 13:45:02 +0100

    Added dates.php

---
+++ dates.php
@@ -0,0 +1,27 @@
+<?php
+
+$datetime = new DateTime();
+$date = date('d-m-y h:i:s');
+$day = date('d');
+$month = date('m');
+# $year = date('y');
+$minute = date('i');
+
+var_dump($datetime);
+
+echo '<br><br>';
+echo $date;
+echo '<br><br>';
+echo $day;
+echo '<br><br>';
+echo $month;
+echo '<br><br>';
+echo $minute;
+echo '<br><br>';
+# echo $year;
+echo '<br><br>';
+echo '<br><br>';
+echo '<br><br>';
+
+
+?>
