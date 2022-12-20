51b695cc9b74759d513e2ff90ebaba91d7544bab
Kriiiiiiiz <kriiiiiiiz@icloud.com>
2022-12-20 13:02:50 +0100

    Added operators.php

---
+++ operators.php
@@ -0,0 +1,83 @@
+<?php
+
+$a = 5;
+$b = 6;
+
+var_dump(1 + 1);
+
+echo '<br><br>';
+
+var_dump(2 - 1);
+
+echo '<br><br>';
+
+var_dump(2 * 2);
+
+echo '<br><br>';
+
+var_dump(10 / 5);
+
+echo '<br><br>';
+
+var_dump(50 % 10);
+
+echo '<br><br>';
+
+var_dump($a == $b);
+
+echo '<br><br>';
+
+var_dump($a === $b);
+
+echo '<br><br>';
+
+var_dump($a != $b);
+
+echo '<br><br>';
+
+var_dump($a !== $b);
+
+echo '<br><br>';
+
+var_dump($a < $b);
+
+echo '<br><br>';
+
+var_dump($a > $b);
+
+echo '<br><br>';
+
+var_dump($a <= $b);
+
+echo '<br><br>';
+
+var_dump($a >= $b);
+
+echo '<br><br>';
+
+var_dump($a <=> $b);
+
+echo '<br><br>';
+
+var_dump($a and $b);
+
+echo '<br><br>';
+
+var_dump($a xor $b);
+
+echo '<br><br>';
+
+var_dump($a xor $b);
+
+echo '<br><br>';
+
+var_dump(! $a);
+
+echo '<br><br>';
+
+var_dump($a && $b);
+
+echo '<br><br>';
+
+var_dump($a || $b);
+?>
