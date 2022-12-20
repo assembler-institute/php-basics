76481f29bec8142891eb472b6018db0ab362499f
Iuliia Shikhanova <yshihanova@gmail.com>
2022-12-20 09:33:41 +0000

    op

--- operators.php
+++ operators.php
@@ -1,3 +1,39 @@
 <?php
+// Operators -, +, *, /, %
+function sumOfBoth(int $a, int $b){
+    return $a + $b;
+}
+echo sumOfBoth(5,7); 
+echo "<br><br>";
+
+function multOfBoth(int $a, int $b){
+    return $a * $b;
+}
+echo multOfBoth(10,7); 
+echo "<br><br>";
+
+function divOfBoth(int $a, int $b){
+    return $a / $b;
+}
+echo divOfBoth(70,7); 
+echo "<br><br>";
+
+function divOfBoth(int $a, int $b){
+    return $a % $b;
+}
+echo divOfBoth(70,7); 
+echo "<br><br>";
+
+// comparison operators
+
+if($a < $b || $a <= $b){
+    return -1;
+}elseif ($a > $b && $a => $b){
+    return 1;
+}elseif ($a == $b){
+    return 0;
+}elseif($a != $b){
+    return $a
+}
 
 ?>
