9658970f7b272bd1d12287b9691b533e45bad350
Javier Pascual <pascualjpt@gmail.com>
2022-12-20 11:38:26 +0100

    End of maths, strings and types !!

--- maths.php
+++ maths.php
@@ -1,3 +1,44 @@
 <?php
 
+/* Punto 1 */
+
+echo abs(-4.2);
+echo "<br>";
+echo abs(5);
+echo "<br>";
+echo abs(-5);
+
+echo "<br><br>";
+
+/* Punto 2 */
+
+echo ceil(4.3);
+echo "<br>";
+echo ceil(9.999);
+echo "<br>";
+echo ceil(-3.14);
+
+echo "<br><br>";
+
+/* Punto 3 */
+
+echo max(2, 3, 1, 6, 7);
+echo "<br>";
+echo max(array(2, 4, 5));
+
+echo "<br><br>";
+
+/* Punto 4 */
+
+echo min(2, 3, 1, 6, 7);
+echo "<br>";
+echo min(array(2, 4, 5));
+
+echo "<br><br>";
+
+/* Punto 5 */
+
+echo (rand(10,100));
+
+
 ?>
--- types.php
+++ types.php
@@ -1,3 +1,70 @@
 <?php
 
+/* boolean */
+
+$a = true;
+$b = false;
+
+echo "<br><br>";
+
+/* integer */
+
+$c = 5985;
+var_dump($c);
+
+echo "<br><br>";
+
+/* float */
+
+$d = 10.365;
+var_dump($d);
+
+echo "<br><br>";
+
+/* String */
+
+$f = "Hello world!";
+$g = 'Hello world!';
+
+echo $f;
+echo "<br>";
+echo $g;
+
+echo "<br><br>";
+
+/* Array */
+
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+
+echo "<br><br>";
+
+/* object */
+
+class Pencil {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+      $this->color = $color;
+      $this->model = $model;
+    }
+    public function message() {
+      return "My pencil is a " . $this->color . " " . $this->model . "!";
+    }
+  }
+  
+  $myPencil = new Pencil("black", "Bic");
+  echo $myPencil -> message();
+  echo "<br>";
+  $myPencil = new Pencil("red", "Pilot");
+  echo $myPencil -> message();
+
+  echo "<br><br>";
+
+/* null */
+
+$h = "Hello world!";
+$i = null;
+var_dump($i);
+
 ?>
