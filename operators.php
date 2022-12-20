1b570eceba5bf13ca51a282fab8e9907e16f7e7b
Joe Alt <joejoyalt@gmail.com>
2022-12-20 10:08:18 +0100

    First 3 php files

---
+++ operators.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File operators.php</title>
+</head>
+
+<body>
+    <h1>library of functionalities php</h1>
+
+    <?php
+
+    var_dump(1 == 2)
+
+    ?>
+
+    <h2>php arithmetic operators</h2>
+
+    <?php
+
+    $a = 3 * 3 % 5;
+    echo $a;
+    echo "<br>";
+
+
+    $b = 1;
+    $c = 2;
+    echo $b = $c += 3;
+    echo "<br>";
+
+
+    $d = 1;
+    echo $d + $d++;
+    echo "<br>";
+
+    ?>
+
+
+
+    <br><br>
+    <h2>php comparison operators</h2>
+
+    <?php
+
+    echo 2 <=> 1;
+    echo "<br>";
+
+
+    echo 2.5 <=> 1.5;
+    echo "<br>";
+
+
+    echo "b" <=> "a";
+    echo "<br>";
+
+
+    echo "zz" <=> "aa";
+    echo "<br>";
+
+
+    echo [1, 2, 3] <=> [1, 2, 4];
+    echo "<br>";
+
+
+    $a = (object) ["a" => "c"];
+    $b = (object) ["a" => "b"];
+    echo $a <=> $b;
+
+    ?>
+
+
+
+    <br><br>
+    <h2>php logical operators</h2>
+
+    <?php
+    
+    function foo() {
+        return 1;
+    }
+
+    $a = (false && foo());
+    $b = (true  || foo());
+    $c = (false and foo());
+    $d = (true  or  foo());
+
+    $e = false || true;
+
+    $f = false or true;
+
+    var_dump($e, $f);
+
+    $g = true && false;
+
+    $h = true and false;
+
+    var_dump($g, $h);
+
+    ?>
+
+</body>
+
+</html>
---
+++ print.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File print.php</title>
+</head>
+
+<body>
+    <h1>library of functionalities php</h1>
+    <h2>php "echo" instructions</h2>
+
+    <?php
+
+        echo "echo does not require parentheses.";
+
+        echo 'This ', 'string ', 'was ', 'made ', 'with multiple parameters.', "\n";
+        echo 'This ' . 'string ' . 'was ' . 'made ' . 'with concatenation.' . "\n";
+
+        echo "hello";
+        echo "world";
+
+        echo "hello", "world";
+
+        echo "This string spans
+        multiple lines. The newlines will be
+        output as well";
+
+        echo "This string spans\nmultiple lines. The newlines will be\noutput as well.";
+
+        $foo = "example";
+        echo "foo is $foo";
+
+        $fruits = ["lemon", "orange", "banana"];
+        echo implode(" and ", $fruits);
+
+        echo 6 * 7;
+        
+    ?>
+
+
+
+    <br><br>
+    <h2>php "print" instructions</h2>
+
+    <?php
+    
+        print "print does not require parentheses.";
+
+        print "hello";
+        print "world";
+
+        print "This string spans
+        multiple lines. The newlines will be
+        output as well";
+
+        print "This string spans\nmultiple lines. The newlines will be\noutput as well.";
+
+        $foo = "example";
+        print "foo is $foo";
+
+        $fruits = ["lemon", "orange", "banana"];
+        print implode(" and ", $fruits);
+
+        print 6 * 7;
+
+        if ( print "hello" ) {
+            echo " world";
+        }
+
+        ( 1 === 1 ) ? print 'true' : print 'false';
+        
+    ?>
+
+
+
+    <br><br>
+    <h2>php "print_r" instructions</h2>
+
+    <?php
+    
+        $a = array ('a' => 'apple', 'b' => 'banana', 'c' => array ('x', 'y', 'z'));
+        print_r ($a);
+        
+    ?>
+
+</body>
+</html>
