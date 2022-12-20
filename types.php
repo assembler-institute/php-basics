2af0ca0ad90b0bb97f24807f8356981dce62cfcf
monica-reverte <monicareverte24@gmail.com>
2022-12-20 09:37:30 +0100

    types

--- types.php
+++ types.php
@@ -10,11 +10,42 @@ $y = false;
 $x = 5985;
 var_dump($x);
 
+#FLOAT -  is a number with a decimal point or a number in exponential form
+
+$x = 10.365;
+var_dump($x);
 
 
+#STRING
+
+echo 'this is a simple string';
+
+#ARRAY 
+
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+
+$array = [
+    "foo" => "bar",
+    "bar" => "foo",
+];
+
+#OBJECT
+
+$fruits = (object)[
+    "fruit 1" => "mango"
+    "fruit 2" => "apple"
+    "fruit 3" => "kiwi"
+];
+
+var_dump($fruits);
 
 
+#NULL
 
+$x = "Hello world!";
+$x = null;
+var_dump($x);
 
 
 
