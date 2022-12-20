2f6d6d95598fdc65244c734a04111b1a07521d4a
Ruben Zafra Traver <rubenreamps@gmail.com>
2022-12-20 11:25:52 +0100

    Dates File Instructions

---
+++ dates.php
@@ -0,0 +1,43 @@
+<?php 
+
+#Date time argument "Y-m-d"
+
+echo "Date with argument 'Y-m-d': <br><br>";
+
+echo "Today is " . date("Y-m-d");
+
+echo "<br><br><br>";
+
+#Date time argument "Y-m-d"
+
+echo "Date with any format 'Y/m/d/l': <br><br>";
+
+echo "Today is " . date("Y/m/d/l");
+
+echo "<br><br><br>";
+
+#Get Current Day 
+
+echo "Current day 'l': <br><br>";
+
+echo "Today is " . date("l");
+
+echo "<br><br><br>";
+
+#Get Current Month in numerical Format
+
+echo "Current Month in numerical Format 'm': <br><br>";
+
+echo "Today is " . date("m");
+
+echo "<br><br><br>";
+
+#Get current minutes with Leading zeros
+
+echo "Current Minutes with leading zeros 'i': <br><br>";
+
+echo "Current minute: " . date("i");
+
+echo "<br><br><br>";
+
+?>
