2733341173c8a0a2b559612924f54283302d72a5
Alvaro Sanchez <alvarosanro89@gmail.com>
2022-12-21 07:35:47 +0100

    new files added

---
+++ arrays.php
@@ -0,0 +1,37 @@
+<?php
+
+$animals = array("dog", "cat", "wolf", "parrot", "monkey");
+
+$numbers = array(1,2.2,3,3.3,4,4.4);
+
+$species = array(
+    array("dog", "wolf", "coyote"),
+    array("cat", "tiger", "lion"),
+    array("macaque", "mandrill", "baboon"),
+);
+
+function lengthArray ($numbers){
+    return count($numbers);
+}
+echo count($numbers);
+echo "<br>";
+
+function arrayMerge() {
+    return array_merge($animals, $numbers);
+};
+print_r(array_merge($animals, $numbers));
+echo "<br>";
+
+function lastElement($animals){
+    return end($animals);
+};
+echo end($animals);
+echo "<br>";
+
+function addElement($numbers){
+    array_push($numbers, 5, 5.5);
+};
+addElement($numbers);
+print_r($numbers);
+
+?>
