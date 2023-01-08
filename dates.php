96d82148fee629f403da28eb411279b325692488
Iñaki García <igardiet@gmail.com>
2023-01-08 14:36:30 +0100

    update

--- dates.php
+++ dates.php
@@ -2,23 +2,49 @@
 
 $date = new DateTime();
 
-# date
-echo date(format: "y-m-d");
+# date 1
+echo date("Y-M-D");
+echo "<br><br>";
+
+# date 2
+echo date("y.m.d");
+echo "<br><br>";
+
+# date 3
+echo date("y/m/d");
+echo "<br><br>";
+
+# date 4
+echo date("Y|M|D");
 echo "<br><br>";
 
 # day
-echo date(format: "d");
+echo date("d");
+echo "<br><br>";
+echo date("D");
 echo "<br><br>";
 
 # weekday name
-echo date(format: "l");
+echo date("l");
+echo "<br><br>";
+echo date("L");
 echo "<br><br>";
 
 # month number
-echo date(format: "m");
+echo date("m");
+echo "<br><br>";
+echo date("M");
 echo "<br><br>";
 
 # minute
-echo date(format: "i");
+echo date("i");
+echo "<br><br>";
+echo date("I");
+echo "<br><br>";
+
+# hour:minutes:seconds
+echo date("H:i:s");
+echo "<br><br>";
+
 
 ?>
